f025fa574d39c647e3ab3a00b9f3f5e48ede1e6a
Renan Pereira <renansoaresinfo@gmail.com>
2018-11-26 19:13:52 -0200

    Cria o setCadastro da classe Usuario

--- usuario.class.php
+++ usuario.class.php
@@ -9,6 +9,21 @@ class Usuario {
 	public function __construct(){}
 
 	//cadastrar Usuário no banco de dados
-	public function setCadastro($pdo, $usuario, $nome, $email, $cargo, $senha, $setor)
+	public function setCadastro($pdo, $usuario, $nome, $email, $cargo, $senha, $setor){
+		$stmt = $pdo->prepare('INSERT INTO usuarios(usuario, nome, email, cargo, senha, idSetor) VALUES (?, ?, ?, ?, ?, ?');
+		$stmt->bindParam(1, $usuario, PDO::PARAM_STR);
+		$stmt->bindParam(2, $nome, PDO::PARAM_STR);
+		$stmt->bindParam(3, $email, PDO::PARAM_STR);
+		$stmt->bindParam(4, $cargo, PDO::PARAM_STR);
+		$stmt->bindParam(5, $senha, PDO::PARAM_STR);
+		$stmt->bindParam(6, $setor, PDO::PARAM_STR);
+
+		//Testa o cadastro.
+		if($stmt->execute()){
+			return "Cadastro realizado com sucesso.";
+		}else{
+			return "Erro no cadastro.";
+		}
+	}
 
 } 
