cc4a5d0b67e27e953255851b8b7f43c1a2b26b58
Renan Pereira <renansoaresinfo@gmail.com>
2018-11-28 15:08:25 -0200

    Arquivos iniciais

--- cadastrar.php
+++ cadastrar.php
@@ -1,96 +1,80 @@
-<?php
-  include "conexao.php";
-  
-  $consulta = $pdo->query('SELECT * FROM setores');
+  <?php
+  include "header.php"
+  ?>
 
-?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<div class="col-12">
+			<h2 style="margin-bottom: 2%; margin-top: 2%;">Cadastro de funcionário</h2>
+			<form name="cadastrar_usuario" method="POST" action="cadastrar_usuario.php" class="form-horizontal">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+				<div class="form-group row">
+					<label class="col-sm-2 col-form-label" for="usuario">Nome de usuário:</label>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" id="usuario" placeholder="Nome de usuário do funcionário" name="usuario">
+					</div>
+				</div>
+			
+				<div class="form-group row">
+					<label class="col-sm-2 col-form-label" for="nome">Nome:</label>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" id="nome" placeholder="Nome completo do funcionário" name="nome">
+					</div>
+				</div>
 
-    <title>Cadastro de Funcionário</title>
-  </head>
-  <body>
-    <div class="container">
-      <div class="col-12">
-        <h2 style="margin-bottom: 2%; margin-top: 2%;">Cadastro de funcionário</h2>
-        <form name="cadastrar_usuario" method="POST" action="cadastrar_usuario.php" class="form-horizontal">
+				<div class="form-group row">
+					<label class="col-sm-2 col-form-label" for="email">E-mail:</label>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" id="email" placeholder="E-mail do funcionário" name="email">
+					</div>
+				</div>
 
-          <div class="form-group row">
-            <label class="col-sm-2 col-form-label" for="usuario">Nome de usuário:</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="usuario" placeholder="Nome de usuário do funcionário" name="usuario">
-            </div>
-          </div>
-      
-          <div class="form-group row">
-            <label class="col-sm-2 col-form-label" for="nome">Nome:</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="nome" placeholder="Nome completo do funcionário" name="nome">
-            </div>
-          </div>
+				<div class="form-group row">
+					<label class="col-sm-2 col-form-label" for="cargo">Cargo:</label>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" id="cargo" placeholder="Ex: Auxiliar administrativo" name="cargo">
+					</div>
+				</div>
 
-          <div class="form-group row">
-            <label class="col-sm-2 col-form-label" for="email">E-mail:</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="email" placeholder="E-mail do funcionário" name="email">
-            </div>
-          </div>
+				<div class="form-group row">
+					<label class="col-sm-2 col-form-label" for="senha">Senha:</label>
+					<div class="col-sm-10">
+						<input type="password" class="form-control" id="senha" placeholder="******" name="senha">
+					</div>
+				</div>
 
-          <div class="form-group row">
-            <label class="col-sm-2 col-form-label" for="cargo">Cargo:</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="cargo" placeholder="Ex: Auxiliar administrativo" name="cargo">
-            </div>
-          </div>
+				<div class="form-group row">
+					<label class="col-sm-2 col-form-label" for="csenha">Confirme a senha:</label>
+					<div class="col-sm-10">
+						<input type="password" class="form-control" id="csenha" placeholder="******" name="csenha">
+					</div>
+				</div>
 
-          <div class="form-group row">
-            <label class="col-sm-2 col-form-label" for="senha">Senha:</label>
-            <div class="col-sm-10">
-              <input type="password" class="form-control" id="senha" placeholder="******" name="senha">
-            </div>
-          </div>
+				<div class="form-group row">
+					<label class="col-sm-2 col-form-label" for="setor">Setor</label>
+					<div class="col-sm-10">
+						<select id="setor" name="setor" class="form-control">
+							<option disabled="disabled" selected>Escolha</option>
+							<?php
+								$consulta = $pdo->query('SELECT * FROM setores');
+								while ($row = $consulta->fetch(PDO::FETCH_ASSOC)){
+							?>
 
-          <div class="form-group row">
-            <label class="col-sm-2 col-form-label" for="csenha">Confirme a senha:</label>
-            <div class="col-sm-10">
-              <input type="password" class="form-control" id="csenha" placeholder="******" name="csenha">
-            </div>
-          </div>
+							<option value="<?php echo $row['idSetor'] ?>"><?php echo $row['nome']; ?></option>
+							<?php } ?>
+						</select>
+					</div>
+				</div>
 
-          <div class="form-group row">
-            <label class="col-sm-2 col-form-label" for="setor">Setor</label>
-              <div class="col-sm-10">
-                <select id="setor" name="setor" class="form-control">
-                <option disabled="disabled" selected>Escolha</option>
-                <?php
-                  while ($row = $consulta->fetch(PDO::FETCH_ASSOC)){
-                ?>
+				<div class="form-group">
+				<button type="submit" class="btn btn-primary btn-lg btn-block">Submit</button>
+				</div>
 
-                <option value="<?php echo $row['idSetor'] ?>"><?php echo $row['nome']; ?></option>
-
-
-                <?php } ?>
-              </select>
-            </div>
-          </div>
-
-          <div class="form-group">        
-          <button type="submit" class="btn btn-primary btn-lg btn-block">Submit</button>
-          </div>
-
-        </form>
-        
-      </div>
-    </div>
+			</form>
+		</div>
+	</div>
 
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
 </html>
---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+	session_start();
+	require "conexao.php";
+
+?>
+<!doctype html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+		<title>Cadastro de Funcionário</title>
+	</head>
+	<body>
+		<div class="container">
