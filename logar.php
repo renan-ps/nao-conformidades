9cec68d6095c1325e85ffdf2da205248592c5708
Renan Pereira <renansoaresinfo@gmail.com>
2018-12-05 20:18:49 -0200

    Sistema de login funcional

--- logar.php
+++ logar.php
@@ -1,16 +1,23 @@
 <?php
-	session_start();
+	
 	include 'conexao.php';
 	include 'usuario.class.php';
 	
-	$usuario = $_POST['usuario'];
+	$user = $_POST['usuario'];
 	$senha = sha1($_POST['senha']);
 
-	$logarUsuario = new Usuario();
-	print_r ($logarUsuario->setLogin($pdo, $usuario, $senha));
+	$usuario = new Usuario();
+	print_r ($usuario->setLogin($pdo, $user, $senha));
 
-	$_SESSION['logado'] = 1;
+	if($usuario->getLogado() == true){
+	$_SESSION['username'] = $usuario->getUsuario();
+	$_SESSION['nome'] = $usuario->getNome();
+	$_SESSION['setor'] = $usuario->getIDSetor();
+	$_SESSION['cargo'] = $usuario->getCargo();
 
-	header('location: login.php');
+	}
+	/*$_SESSION['setor'] = ($usuario->getIDSetor());*
+
+	/*header('location: login.php');*/
 
 ?>
--- usuario.class.php
+++ usuario.class.php
@@ -1,10 +1,12 @@
 <?php 
 class Usuario { 
+	private $idUsuario;
 	private $usuario; 
 	private $nome;
 	private $email;
 	private $idSetor;
 	private $cargo;
+	private $logado;
 
 	public function __construct(){}
 
@@ -43,13 +45,72 @@ class Usuario {
 		$stmt -> execute();
 
 		if($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-			return "ok";
+			$this->setIDUsuario($row['idUsuario']);
+			$this->setUsuario($row['usuario']);
+			$this->setNome($row['nome']);
+			$this->setEmail($row['email']);
+			$this->setIDSetor($row['idSetor']);
+			$this->setCargo($row['cargo']);
+			$this->setLogado(true);
 		}
 		else{
 			return "erro";
 		}
 	}
 
+	//Sets básicos
+	public function setUsuario($usuario){
+		$this->usuario = $usuario;
+	}
+
+	public function setIDUsuario($idUsuario){
+		$this->idUsuario = $idUsuario;
+	}
+
+	public function setNome($nome){
+		$this->nome = $nome;
+	}
+
+	public function setEmail($email){
+		$this->email = $email;
+	}
+
+	public function setIDSetor($idSetor){
+		$this->idSetor = $idSetor;
+	}
+
+	public function setCargo($cargo){
+		$this->cargo = $cargo;
+	}
+
+	public function setLogado($log){
+		$this->logado = $log;
+	}
+
+	//gets basicos
+	public function getUsuario(){
+		return $this->usuario;
+	}
+
+	public function getNome(){
+		return $this->nome;
+	}
+
+	public function getEmail(){
+		return $this->email;
+	}
+
+	public function getIDSetor(){
+		return $this->idSetor;
+	}
+
+	public function getCargo(){
+		return $this->cargo;
+	}
+
+	public function getLogado(){
+		return $this->cargo;
+	}
 
 
 }
